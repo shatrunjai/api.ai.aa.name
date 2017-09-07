88028f74333510f2013cac127f9284dcd9a496ca
Shatrunjai Singh <shatrunjai@gmail.com>
2017-09-07 16:02:49 -0400

    Add files via upload

---
+++ api.aiwebapp_aa_title.py
@@ -0,0 +1,64 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+#!/usr/bin/env python
+
+import urllib
+import json
+import os
+
+from flask import Flask
+from flask import request
+from flask import make_response
+
+# Flask app should start in global layout
+app = Flask(__name__)
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+
+    print("Request:")
+    print(json.dumps(req, indent=4))
+
+    res = makeWebhookResult(req)
+
+    res = json.dumps(res, indent=4)
+    print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+def makeWebhookResult(req):
+    if req.get("result").get("action") != "aa.role":
+        return {}
+    result = req.get("result")
+    parameters = result.get("parameters")
+    zone = parameters.get("given-name")
+
+    cost = {'Vishwa':'Assistant Vice President of Advanced Analytics at John Hancock', 'Jun':'Senior Director, Advanced Analytics at John Hancock', 'Jai':'Senior Data Scientist, Advanced Analytics at John Hancock'}
+
+    speech = "The title for " + zone + " is " + str(cost[zone]) + ", Thank you. "
+
+    print("Response:")
+    print(speech)
+
+    return {
+        "speech": speech,
+        "displayText": speech,
+        #"data": {},
+        # "contextOut": [],
+        "source": "apiai-onlinestore-shipping"
+    }
+
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+
+    print "Starting app on port %d" % port
+
+    app.run(debug=True, port=port, host='0.0.0.0')
+
